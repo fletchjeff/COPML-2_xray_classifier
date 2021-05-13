9157e2338f3dd5f72f5eed8456c8ec8ac2609691
Jeff Fletcher <jfletcher@cloudera.com>
2021-05-13 15:36:22 +0000

    2nd Model

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,1 +1,2 @@
+#ln -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.11.0.1.105 /usr/local/cuda-11.1/targets/x86_64-linux/lib/libcusolver.so.10
 pip3 install --upgrade -r requirements.txt
